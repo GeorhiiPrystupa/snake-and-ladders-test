fd854fe6ad2ed1355fd808233535125faa89ae1a
Georhii Prystupa <georhii.prystupa@gmail.com>
2021-03-05 13:41:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake and Ladders
+
+## run application
+To run application locally use
+"mvn spring-boot:run". This command will serve the app on port 8080.
---
+++ src/main/kotlin/com/georhii/codingtask/CodingtaskApplication.kt
@@ -0,0 +1,11 @@
+package com.georhii.codingtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodingtaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodingtaskApplication>(*args)
+}
